5d773fa8e30c2451861a58179179c4d34b55977b
mceciabate <m.cecilia.abate@gmail.com>
2023-05-22 22:53:17 -0300

    Clase 8

--- clase8/main.go
+++ clase8/main.go
@@ -11,9 +11,9 @@ func main() {
 	fmt.Printf("La persona se llama %s, y tiene %d años \n", nombre, edad)
 	var frase string = fmt.Sprintf("Hola %s %d", "mundo", 10)
 	fmt.Println(frase)
-	// var mensajeConsola string
-	// fmt.Scan(&mensajeConsola)
-	// fmt.Println("El mensaje recibido por consola es :", mensajeConsola)
+	var mensajeConsola string
+	fmt.Scan(&mensajeConsola)
+	fmt.Println("El mensaje recibido por consola es :", mensajeConsola)
 	var nombre_completo, apellido string
 	var telefono int
 	fmt.Scanf("%s %s %d", &nombre_completo, &apellido, &telefono)
