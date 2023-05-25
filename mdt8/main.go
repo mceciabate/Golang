093f8d83e25c5005f97fddd1157db8dbb5067866
mceciabate <m.cecilia.abate@gmail.com>
2023-05-25 17:16:26 -0300

    terminado

--- mdt8/main.go
+++ mdt8/main.go
@@ -27,8 +27,9 @@ Código
 Nombre
 PrecioActual
 CantidadActual
-	Insertar al menos cuatro productos.
-Generar otro archivo CSV, llamado estimaciones.csv. Este tendrá los resultados de la suma de todos los productos de cada una de las categorías.
+	Insertar al menos cuatro productos.*/
+//PARTE II
+/*Generar otro archivo CSV, llamado estimaciones.csv. Este tendrá los resultados de la suma de todos los productos de cada una de las categorías.
 Imprimir todos los estimativos por consola. Ejemplo:
 Categoría			EstimativoPorCategoría
 Construcción				60.700
@@ -48,7 +49,6 @@ type Producto struct {
 type ListaProductos []Producto
 
 var ListaElectro = ListaProductos{
-	p1,
 	p2,
 }
 
@@ -66,26 +66,26 @@ var p1 = Producto{
 	Codigo:         1,
 	Nombre:         "Pava Electrica",
 	PrecioActual:   10000.00,
-	CantidadActual: 20,
+	CantidadActual: 2,
 }
 var p2 = Producto{
 	Codigo:         2,
 	Nombre:         "Lavarropas",
 	PrecioActual:   200000.00,
-	CantidadActual: 15,
+	CantidadActual: 1,
 }
 var p3 = Producto{
 	Codigo:         3,
 	Nombre:         "Mesa",
 	PrecioActual:   35000.00,
-	CantidadActual: 35,
+	CantidadActual: 2,
 }
 
 var p4 = Producto{
 	Codigo:         4,
 	Nombre:         "Sillon",
 	PrecioActual:   75000.00,
-	CantidadActual: 4,
+	CantidadActual: 1,
 }
 
 var p5 = Producto{
@@ -99,6 +99,14 @@ var p6 = Producto{
 	Nombre:         "Martillo",
 	PrecioActual:   3000.00,
 	CantidadActual: 12,
+}
+
+func (lp ListaProductos) calculadoraEstimaciones() (float64, error) {
+	var acumulador float64
+	for _, v := range lp {
+		acumulador += float64(v.CantidadActual) * (v.PrecioActual)
+	}
+	return acumulador, nil
 }
 
 func main() {
@@ -115,36 +123,95 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//ESTIMACIONES
+	estimacionMuebles, err := ListaMuebles.calculadoraEstimaciones()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(estimacionMuebles)
+	estimacionHerramientas, err := ListaHerramientas.calculadoraEstimaciones()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(estimacionHerramientas)
+	estimacionElectro, err := ListaElectro.calculadoraEstimaciones()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(estimacionElectro)
 
-	listadoCompleto := []string{"001", "Electrodomesticos", string(lista1), ";", "002", "Muebles", string(lista2), ";", "003", "Herramientas", string(lista3)}
+	//CONVERTIR A STRING LOS FLOAT RETORNADOS
+	stringElectro := fmt.Sprintf("%v", estimacionElectro)
+	stringMuebles := fmt.Sprintf("%v", estimacionMuebles)
+	stringHerramientas := fmt.Sprintf("%v", estimacionHerramientas)
+	fmt.Println("----------ESTIMACIONES AS STRING-------------")
+	fmt.Println(stringElectro, stringMuebles, stringHerramientas)
+
+	//ARMO Y CONVIERTO LAS LISTAS EN STRINGS
+	listadoCompleto := []string{"001", "Electro", string(lista1), ";", "002", "Muebles", string(lista2), ";", "003", "Herram", string(lista3)}
 	listadoAsString := strings.Join(listadoCompleto, " ")
+	listadoEstimaciones := []string{"Categoria", "Estimados", ";", "Electro", stringElectro, ";", "Muebles", stringMuebles, ";", "Herram", stringHerramientas}
+	listadoEstimacionesAsString := strings.Join(listadoEstimaciones, " ")
+
 	fmt.Println("----------LISTADO AS STRING-------------")
-
 	fmt.Println(listadoAsString)
+	fmt.Println(listadoEstimacionesAsString)
 
-	//CREO EL ARCHIVO
-	f, err := os.Create("data.csv")
+	//CREO EL ARCHIVO PARA CATEGORIAS
+	f, err := os.Create("categorias.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//ESCRIBO EL ARCHIVO CON EL JSON
+	//CREO EL ARCHIVO PARA ESTIMACIONES
+	f2, err := os.Create("estimaciones.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ESCRIBO EL ARCHIVO CATEGORIAS
 	if _, err = f.WriteString(listadoAsString); err != nil {
 		log.Fatal(err)
 	}
 
-	//LEO EL ARCHIVO
-	rawData, err := os.ReadFile("./data.csv")
+	//ESCRIBO EL ARCHIVO ESTIMACIONES
+	if _, err = f2.WriteString(listadoEstimacionesAsString); err != nil {
+		log.Fatal(err)
+	}
+
+	//LEO EL ARCHIVO CATEGORIAS
+	rawData, err := os.ReadFile("./categorias.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//LEO EL ARCHIVO ESTIMACIONES
+	rawDataEstimacion, err := os.ReadFile("./estimaciones.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//PASO A STRING LAS LECTURAS OBTENIDAS
 	data := strings.Split(string(rawData), "; ")
-	fmt.Println("---------SALIDA SPLIT--------------")
-	fmt.Println(data)
+	dataEstimacion := strings.Split(string(rawDataEstimacion), "; ")
+	fmt.Println("--------DATOS OBTENIDOS DE LAS LECTURAS--------------")
+	fmt.Println(data, "\n", dataEstimacion)
+
 	fmt.Println("-----------------FOR---------------------------")
+
+	fmt.Println("--------TABLA CATEGORIAS---------")
 	for _, v := range data {
 		line := strings.Split(v, " ; ")
-		fmt.Printf("%s\n", line[0])
+		line2 := strings.Split(line[0], " ")
+		fmt.Printf("%s\t%s\t%s\t\n", line2[0], line2[1], line2[2])
+	}
+
+	fmt.Println("--------TABLA ESTIMACIONES---------")
+	for _, v := range dataEstimacion {
+		line := strings.Split(v, " ; ")
+		line2 := strings.Split(line[0], " ")
+		fmt.Printf("%s\t\t\t%s\n", line2[0], line2[1])
 
 	}
+
 }
